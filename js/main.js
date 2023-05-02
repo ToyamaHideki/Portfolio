c7269b8571d89393b169f0f3dd15ff2e6cd7044e
ToyamaHideki <96602926+ToyamaHideki@users.noreply.github.com>
2023-05-02 13:09:53 +0900

    機能完了、不要なファイルの削除

--- js/main.js
+++ js/main.js
@@ -1,84 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  ScrollReveal().reveal(".section__title", {
-    duration: 1600,
-    origin: "top",
-    distance: "20px",
-    reset: false,
-  });
-  ScrollReveal().reveal(".skill__wrap", {
-    duration: 1600,
-    delay:100,
-    origin: "top",
-    distance: "20px",
-    reset: false,
-  });
-  ScrollReveal().reveal(".timeline li", {
-    duration: 1600,
-    delay:100,
-    origin: "top",
-    distance: "20px",
-    reset: false,
-  });
-  ScrollReveal().reveal(".image-container", {
-    duration: 1600,
-    delay:500,
-    origin: "top",
-    distance: "20px",
-    reset: false,
-  });
-  ScrollReveal().reveal(".profile-text-container", {
-    duration: 1600,
-    delay:600,
-    origin: "top",
-    distance: "20px",
-    reset: false,
-  });
-  ScrollReveal().reveal(".profile-list", {
-    duration: 1600,
-    delay:700,
-    origin: "top",
-    distance: "20px",
-    reset: false,
-  });
+  revealInit();
+  mainTitleShadowInit();
 
-  ScrollReveal().reveal(".maintitle", {
-    duration: 4000,
+  setTimeout(()=>{dispCharTitle(".subtitle")},2000)
+
+  window.addEventListener("scroll", function () {
+    ScrollTimelineAnime(); // 線が伸びる関数を呼ぶ
   });
+});
 
-  setTimeout(() => {
-    dispCharSubtitle();
-  }, 2000);
+window.addEventListener("load", function () {
+  ScrollTimelineAnime(); // 線が伸びる関数を呼ぶ
+});
 
-  const cursor = document.querySelector(".custom-cursor");
-  const links = document.querySelectorAll("a");
-
-  gsap.set(cursor, { xPercent: -50, yPercent: -50 });
-
-  links.forEach((link) => {
-    link.addEventListener("mouseenter", () => {
-      gsap.to(cursor, {
-        duration: 0.3,
-        scale: 2,
-      });
-    });
-
-    link.addEventListener("mouseleave", () => {
-      gsap.to(cursor, {
-        duration: 0.3,
-        scale: 1,
-      });
-    });
-  });
-
-  document.addEventListener("mousemove", (event) => {
-    gsap.to(cursor, {
-      duration: 0.1,
-      x: event.clientX,
-      y: event.clientY,
-      rotation: "+=90",
-    });
-  });
-
+// 影の動作をGSAPで制御
+const mainTitleShadowInit = function () {
   const title = document.querySelector(".maintitle");
   const shadow = document.querySelector(".title-shadow");
   const deepShadow = document.querySelector(".title-deep-shadow");
@@ -115,65 +51,143 @@ document.addEventListener("DOMContentLoaded", () => {
       duration: 2,
     });
   });
+};
 
-  // Career コンポーネントのスクロール検知とライン描写の設定
-  function ScrollTimelineAnime() {
-    const listItems = document.querySelectorAll(".timeline li");
-    const startPoint = 100; // 線をスタートさせる位置を指定 ※レイアウトによって調整してください
-
-    listItems.forEach((item) => {
-      const elemPos = item.offsetTop; // 上からの高さ取得
-      const scroll = window.pageYOffset; // スクロール値取得
-      const windowHeight = window.innerHeight; // windowの高さ取得
-
-      if (scroll >= elemPos - windowHeight - startPoint) {
-        const H = item.offsetHeight; // liの余白と高さを含めた数値を取得
-        const percent = ((scroll + startPoint - elemPos) / (H / 2)) * 100; // liの余白と高さの半分で線を100％に伸ばす
-
-        // 100% を超えたらずっと 100% を入れ続ける
-        const borderLine = item.querySelector(".border-line");
-        if (percent > 100) {
-          borderLine.style.height = "100%"; //CSSでパーセント指定
-        } else {
-          borderLine.style.height = percent + "%"; //CSSでパーセント指定
-        }
-      }
-    });
-  }
-
-  window.addEventListener("scroll", function () {
-    ScrollTimelineAnime(); // 線が伸びる関数を呼ぶ
+// スクロール処理を関数にまとめる
+const revealInit = function () {
+  ScrollReveal().reveal(".section__title", {
+    duration: 1600,
+    delay: 50,
+    origin: "top",
+    distance: "20px",
+    reset: false,
   });
-});
 
-window.addEventListener("load", function () {
-  ScrollTimelineAnime(); // 線が伸びる関数を呼ぶ
-});
+  // ========================== PROFILE ==========================
+  ScrollReveal().reveal(".profile__imageContainer", {
+    duration: 1600,
+    delay: 100,
+    origin: "left",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__top", {
+    duration: 1600,
+    delay: 100,
+    origin: "right",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__desc", {
+    duration: 1600,
+    delay: 300,
+    origin: "right",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__list__fst", {
+    duration: 1600,
+    delay: 500,
+    origin: "right",
+    distance: "10px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__list__snd", {
+    duration: 1600,
+    delay: 600,
+    origin: "right",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__list__thd", {
+    duration: 1600,
+    delay: 700,
+    origin: "right",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__list__frth", {
+    duration: 1600,
+    delay: 800,
+    origin: "right",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".reveal__list__fifth", {
+    duration: 1600,
+    delay: 900,
+    origin: "right",
+    distance: "20px",
+    reset: false,
+  });
 
 
+  ScrollReveal().reveal(".timeline li", {
+    duration: 1600,
+    delay: 100,
+    origin: "top",
+    distance: "20px",
+    reset: false,
+  });
+  ScrollReveal().reveal(".image-container", {
+    duration: 1600,
+    delay: 500,
+    origin: "top",
+    distance: "20px",
+    reset: false,
+  });
 
+  ScrollReveal().reveal(".profile-text-container", {
+    duration: 1600,
+    delay: 600,
+    origin: "top",
+    distance: "20px",
+    reset: false,
+  });
+
+
+  ScrollReveal().reveal(".maintitle", {
+    duration: 4000,
+  });
+};
+
+// 文字を分割して表示する処理
 // タイピングされたように表示される
-const dispCharSubtitle = () => {
-  const subtitle = document.querySelector(".subtitle");
-  const subtitleText = subtitle.textContent;
+const dispCharTitle = (className) => {
+  const charSet = document.querySelector(className);
+  const charSetText = charSet.textContent;
   const delay = 100;
 
-  subtitle.textContent = "";
-
-  for (let i = 0; i < subtitleText.length; i++) {
+  charSet.textContent = "";
+  for (let i = 0; i < charSetText.length; i++) {
     setTimeout(() => {
-      subtitle.textContent += subtitleText[i];
-      subtitle.style.opacity = 1;
+      charSet.textContent += charSetText[i];
+      charSet.style.opacity = 1;
     }, delay * i);
   }
 };
 
-// スマホの傾きで影の描写を反映する
-window.addEventListener("devicemotion", (e) => {
-  const { x, y } = e.accelerationIncludingGravity;
-  gsap.to(shadow, {
-    x: (-x * 0.1) / 7,
-    y: (-y * 0.1) / 7,
-    duration: 4,
+// Career コンポーネントのスクロール検知とライン描写の設定
+const ScrollTimelineAnime = function () {
+  const listItems = document.querySelectorAll(".timeline li");
+  const startPoint = 100; // 線をスタートさせる位置を指定 ※レイアウトによって調整してください
+
+  listItems.forEach((item) => {
+    const elemPos = item.offsetTop; // 上からの高さ取得
+    const scroll = window.pageYOffset; // スクロール値取得
+    const windowHeight = window.innerHeight; // windowの高さ取得
+
+    if (scroll >= elemPos - windowHeight - startPoint) {
+      const H = item.offsetHeight; // liの余白と高さを含めた数値を取得
+      const percent = ((scroll + startPoint - elemPos) / (H / 2)) * 100; // liの余白と高さの半分で線を100％に伸ばす
+
+      // 100% を超えたらずっと 100% を入れ続ける
+      const borderLine = item.querySelector(".border-line");
+      if (percent > 100) {
+        borderLine.style.height = "100%"; //CSSでパーセント指定
+      } else {
+        borderLine.style.height = percent + "%"; //CSSでパーセント指定
+      }
+    }
   });
-});
+};
